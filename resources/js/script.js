ea00a0ff867bd0918f44f3b74085c3d72bd70bdd
Lucas Fernandes <lucasfeol94@gmail.com>
2018-10-27 11:19:30 -0200

    Desenvolvendo registro de nomes

--- resources/js/script.js
+++ resources/js/script.js
@@ -15,8 +15,19 @@ function addItemToList(listId, input, inputList) {
     if(myList.childElementCount >= 9) return;
     const node = document.createElement('li');
     const span = document.createElement('span');
+    const btnDel = document.createElement('button');
+    btnDel.innerHTML = 'Apagar';
+    btnDel.className = 'btn btn-sm btn-danger float-right';
+    btnDel.addEventListener("click", () => {
+        names = document.getElementById(inputList).value.split(',');
+        index = names.indexOf(name);
+        names.splice(index, 1);
+        document.getElementById(inputList).value = names.toString();
+        myList.removeChild(node);
+    });
     span.innerHTML = name;
     node.appendChild(span);
+    node.appendChild(btnDel);
     myList.appendChild(node);
     document.getElementById(input).value = '';
 
@@ -44,9 +55,30 @@ function registerName() {
     const myList = document.getElementById('registeredNames');
     const node = document.createElement('li');
     const span = document.createElement('span');
+    const btnDel = document.createElement('button');
+    btnDel.innerHTML = 'Apagar';
+    btnDel.className = 'btn btn-sm btn-danger float-right';
+    btnDel.addEventListener("click", () => {
+        myList.removeChild(node);
+    });
     span.innerHTML = fullName;
     node.appendChild(span);
+    node.appendChild(btnDel);
     myList.appendChild(node);
+
+    const nameUse = document.getElementById('nameUse').value;
+    const nameRepresentation = document.getElementById('nameRepresentation').value;
+
+    const nameInfo = {
+        full_name: fullName,
+        use: nameUse,
+        represetation: nameRepresentation
+    }
+    saveNameLocalStorage(nameInfo);
+}
+
+function removeNamesFormInput() {
+
 }
 
 function addEnterEvenToButtons() {
@@ -57,4 +89,10 @@ function addEnterEvenToButtons() {
             document.getElementById("titleInput").focus();
         }
     });
+}
+
+function saveNameLocalStorage(nameInfo) {
+    currentList = JSON.parse(localStorage.getItem('names')) || [];
+    currentList.push(nameInfo);
+    localStorage.setItem('names', JSON.stringify(currentList));
 }
