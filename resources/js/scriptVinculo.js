9dd5b461fac8c664c1952c6878843615a89a7060
Lucas Fernandes <lucasfeol94@gmail.com>
2018-11-10 16:33:10 -0200

    Teste botao editar 2

--- resources/js/scriptVinculo.js
+++ resources/js/scriptVinculo.js
@@ -73,7 +73,7 @@ function addBondTableRow(id, cellInfoList, update) {
         });
 
         const btnEdit = document.createElement('button');
-        btnEdit.type = 'button';
+        btnEdit.setAttribute('type', 'button');
         btnEdit.innerHTML = 'Editar';
         btnEdit.className = 'btn btn-sm btn-primary float-right mr-1';
         btnEdit.setAttribute('data-target', '#cadastroVinculoModal');
